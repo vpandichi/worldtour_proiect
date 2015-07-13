6a0f072c5d63522fe3df5365d1c01b0a8d1093e8
Vlad Pandichi <vladz0r9@yahoo.com>
2015-07-13 23:48:29 +0300

    Initial commit - created provisory landing page

---
+++ worldtour/public/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to worldtour! Your free touristic guide based on real stories.</title>
+	<link rel="stylesheet" href="/sites/worldtour/public/styles/main.css">
+</head>
+<body>
+	<div id="lpage">
+		<div id="logo"><img src="/sites/worldtour/public/img/provisory-logo.gif" alt=""></div>
+			<video autoplay loop muted class="bgvideo" id="bgvideo">
+				<source src="/sites/worldtour/public/img/slideshow.mp4" type="video/mp4">
+			</video>
+			<nav>
+				<ul class="noop">
+					<li><a href="#about">about</a></li>
+					<li><a href="#recommendations">recommendations</a></li>
+					<li><a href="#stories">stories</a></li>
+					<li><a href="#contact">contact</a></li>
+					<li><a href="#login">log in</a></li>
+					<li><a href="#lang">en</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+	<div id="header"></div>
+	<div id="page-content"></div>
+	<div id="footer"></div>
+	<script type="text/javascript" src="/sites/worldtour/public/scripts/scroll.js"></script>
+</body>
+</html>
